56b4e4e64da9bd78aeeb4d3cb4a24a3ab95c23c8
Douglas B <33129984+CasualSloth@users.noreply.github.com>
2025-10-06 19:38:26 -0400

    Add DbContext for CouponAPI
    Added DbContext for the CouponAPI and updated the Coupon object to have correct attributes

---
+++ Mango.Services.CouponAPI/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Mango.Services.CouponAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.CouponAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        // Pass constructor arguments over to base class - Default setting required by Entity Framework Core
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+
+        // Used to Query and Save instances of Coupon to database. Name of the DbSet will be the name of the table in the database
+        public DbSet<Coupon> Coupons { get; set; }
+    }
+}
