476bc2892b41bea2ed074ac3f24bb765b7fe5b15
Douglas B <33129984+CasualSloth@users.noreply.github.com>
2025-10-06 23:22:43 -0400

    Create basic GET API Endpoint
    Created a basic api endpoing to get coupons. Also cleaned out the default WeatherForecast API. The new nuget package was added to test the EF Core controller generation

---
+++ Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
@@ -0,0 +1,47 @@
+﻿using Mango.Services.CouponAPI.Data;
+using Mango.Services.CouponAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.CouponAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CouponAPIController : ControllerBase
+    {
+        private readonly AppDbContext _dbContext;
+
+        public CouponAPIController(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public object Get()
+        {
+            try
+            {
+                IEnumerable<Coupon> couponList = _dbContext.Coupons.ToList();
+                return couponList;
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public object Get(int id)
+        {
+            try
+            {
+                Coupon coupon = _dbContext.Coupons.First(u => u.CouponId == id);
+                return coupon;
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
