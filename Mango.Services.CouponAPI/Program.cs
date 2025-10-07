be391cf1fdb2d12b15c39778379ebb083e7ec81f
Douglas B <33129984+CasualSloth@users.noreply.github.com>
2025-10-06 20:45:22 -0400

    Create initial migration to database for Coupons table
    Created a migration and updated database with the Coupons table.

--- Mango.Services.CouponAPI/Program.cs
+++ Mango.Services.CouponAPI/Program.cs
@@ -1,8 +1,17 @@
+using Mango.Services.CouponAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddDbContext<AppDbContext>(option =>
+{
+    option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
 builder.Services.AddControllers();
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddSwaggerGen();
 
