4f96e1df695b0f2382bcebe4aba24782f745092a
Douglas B <33129984+CasualSloth@users.noreply.github.com>
2025-10-08 17:39:00 -0400

    Added Controller and View for Delete Coupon
    Added a new Controller method and view for deleting a coupon. This also required a change to the Route in the CouponAPIController

--- Mango.Web/Controllers/CouponController.cs
+++ Mango.Web/Controllers/CouponController.cs
@@ -47,5 +47,29 @@ namespace Mango.Web.Controllers
 
             return View(model); // Return to the Create page with our current model
         }
+        
+        public async Task<IActionResult> CouponDelete(int couponId)
+        {
+            ResponseDto? response = await _couponService.GetCouponByIdAsync(couponId);
+            if (response != null && response.IsSuccessful && response.Result != null)
+            {
+                CouponDto? model = JsonConvert.DeserializeObject<CouponDto>(Convert.ToString(response.Result)!);
+                return View(model);
+            }
+
+            return NotFound(); // .NET's default NotFound page
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CouponDelete(CouponDto couponDto)
+        {
+            ResponseDto? response = await _couponService.DeleteCouponAsync(couponDto.CouponId);
+            if (response != null && response.IsSuccessful)
+            {
+                return RedirectToAction(nameof(CouponIndex)); // Basically calls CouponIndex so we can see the new Coupon
+            }
+
+            return View(couponDto); // Return to the CouponDelete page with our current model
+        }
     }
 }
