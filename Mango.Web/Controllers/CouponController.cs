dbc7b636e1e85082aca8bd28054c3cf8ef1979de
Douglas B <33129984+CasualSloth@users.noreply.github.com>
2025-10-07 22:14:52 -0400

    Create CouponIndex View and link Model
    Created the CouponIndex view and linked up the list of CouponDto objects so we can properly display them. Also hard-coded the CouponAPI route to api/coupon

---
+++ Mango.Web/Controllers/CouponController.cs
@@ -0,0 +1,30 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Service.IService;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Mango.Web.Controllers
+{
+    public class CouponController : Controller
+    {
+        private readonly ICouponService _couponService;
+        public CouponController(ICouponService couponService)
+        {
+            _couponService = couponService;
+        }
+
+        // Invoked when our item is pressed in the dropdown
+        public async Task<IActionResult> CouponIndex()
+        {
+            List<CouponDto>? list = new();
+
+            ResponseDto? response = await _couponService.GetAllCouponsAsync();
+            if (response != null && response.IsSuccessful && response.Result != null)
+            {
+                list = JsonConvert.DeserializeObject<List<CouponDto>>(Convert.ToString(response.Result)!);
+            }
+
+            return View(list); // Returns the list to the view (CouponIndex.cshtml - Name of this method) for display
+        }
+    }
+}
